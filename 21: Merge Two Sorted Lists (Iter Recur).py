8ea1b9f8201dbed9d2e2ae638ae2cedf64b41117
Dewang Patil <71816513+DewangPatil30@users.noreply.github.com>
2022-02-24 21:39:27 +0530

    Update and rename 21: Merge Two Sorted Lists.py to 21: Merge Two Sorted Lists (Iter Recur).py
    LeetCode 21: https://leetcode.com/problems/merge-two-sorted-lists/

--- 21: Merge Two Sorted Lists (Iter Recur).py
+++ 21: Merge Two Sorted Lists (Iter Recur).py
@@ -22,3 +22,28 @@ class Solution:
             else:
                 newlistptr.next = l2
             return newlist.next
+        NewList = ListNode()
+        
+############### Rucursive ####################
+		
+        def Merge(NewList,node1,node2):
+
+                if node1 == None and node2 == None:
+                    return
+
+                if node1 == None and node2 != None:
+                    return node2
+
+                if node2 == None and node1 != None:
+                    return node1
+
+                if node1.val <= node2.val:
+                    NewList = node1
+                    NewList.next = Merge(NewList, node1.next,node2)
+
+                if node1.val > node2.val:
+                    NewList = node2
+                    NewList.next = Merge(NewList, node1,node2.next)
+
+                return NewList
+            
