612e591d9b97bf747d5d7d0acb03746bb4debc21
Dewang Patil <71816513+DewangPatil30@users.noreply.github.com>
2022-03-01 06:18:44 +0530

    Update 338: Counting Bits (Converting to binary).py
    num % 2 -- num // 2 till num > 0 
    LeetCode 338: https://leetcode.com/problems/counting-bits/

--- 338: Counting Bits (Converting to binary).py
+++ 338: Counting Bits (Converting to binary).py
@@ -1,5 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        
+        # Sol 0: Using DP:
+        
+        res = [0]
+        for i in range(1, n+1):
+            res.append(res[i>>1] + i%2)
+        return res
 
         # Sol 1: Raw Logic O(n2)
         
