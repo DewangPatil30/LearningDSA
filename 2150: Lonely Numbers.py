0b9274f002f694b6e048359c47b51cdee0630912
Dewang Patil <71816513+DewangPatil30@users.noreply.github.com>
2022-04-07 04:59:50 +0530

    Update 2150: Lonely Numbers.py

--- 2150: Lonely Numbers.py
+++ 2150: Lonely Numbers.py
@@ -1,22 +1,26 @@
+"""
+Approach: 
+    !. Use Map: TC:O(N) SC:O(N)
+        a. store each distinct ele in map with frequency
+        b travase in map if freq of ele is 1 and ele +- 1 not in m then res.append(ele)
+        return res
+    
+    LeetCode 2150: https://leetcode.com/problems/find-all-lonely-numbers-in-the-array/
+"""
+
 class Solution:
     def findLonely(self, nums: List[int]) -> List[int]:
         
-        m = dict()
-        setn = set()
+        m = {}
         
-        for i in nums:
-            if i not in setn:
-                setn.add(i)
-                m[i] = 1
-            else:
-                if i in m:
-                    m.pop(i)
-                    
-        nums = set(nums)
-        setn = set(m.keys())
-        for i in setn:
-            if i+1 in nums or i-1 in nums:
-                if i in m:
-                    m.pop(i)
-                    
-        return list(m.keys())
+        for v in nums:
+            if v not in m: m[v] = 0
+            m[v] += 1
+        
+        res = []
+        for ele in m:
+            if m[ele] == 1 and ele-1 not in m and ele+1 not in m:
+                res.append(ele)
+                
+        return res
+                 
