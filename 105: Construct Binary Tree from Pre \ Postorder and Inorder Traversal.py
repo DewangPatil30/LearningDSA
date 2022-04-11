fd75203b75379cd557a501c58a4c5cdb5052fdfa
Dewang Patil <71816513+DewangPatil30@users.noreply.github.com>
2022-04-12 00:43:31 +0530

    Update and rename 105: Construct Binary Tree from Preorder and Inorder Traversal.py to 105: Construct Binary Tree from Pre \ Postorder and Inorder Traversal.py

--- 105: Construct Binary Tree from Pre \ Postorder and Inorder Traversal.py
+++ 105: Construct Binary Tree from Pre \ Postorder and Inorder Traversal.py
@@ -11,22 +11,26 @@ Approach: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
         
         return root
 
+
+        FOR POSTORDER:
+        Same as preorder just instead of pop(0) we use pop() and call for root.right will come first than for left
+
     LeetCode 105: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
     YT Strivers: https://youtu.be/aZNaLrVebKQ
 """
 
+# FOR PREORDER
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         
         m = {k: i for i, k in enumerate(inorder)}
-        preStack = preorder.copy()
         
         def constructTree(l, r):
             
             if l > r:
                 return 
             
-            root = TreeNode(preStack.pop(0))
+            root = TreeNode(preorder.pop(0))
             mid = root.val
             
             root.left = constructTree(l, m[mid]-1)
@@ -38,4 +42,22 @@ class Solution:
         return constructTree(0, len(preorder)-1)
         
         
+# FOR PREORDER
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         
+        ind = {k:i for i, k in enumerate(inorder)}
+        
+        def constructTree(l, r):
+            if l > r: return 
+            
+            root = TreeNode(postorder.pop())  # Change 1
+            mid = root.val
+            
+            root.right = constructTree(ind[mid]+1, r) #Change 2
+            root.left = constructTree(l, ind[mid]-1)
+            
+            return root
+            
+        
+        return constructTree(0, len(postorder)-1)
