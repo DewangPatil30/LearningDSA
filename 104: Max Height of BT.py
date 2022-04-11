21bb4bef343ac5e831c858a41468c2b48704698d
Dewang Patil <71816513+DewangPatil30@users.noreply.github.com>
2022-04-11 19:56:58 +0530

    Update 104: Max Height of BT.py

--- 104: Max Height of BT.py
+++ 104: Max Height of BT.py
@@ -1,3 +1,15 @@
+"""
+Approach:
+        
+        1. post order trav: if not root return 0
+            l = fn(root.left)
+            r = fn(root.right)
+            
+            return 1 + max(l,r)
+
+    LeetCode 104: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+"""
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         
