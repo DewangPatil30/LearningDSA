7976a968d8e3b9f8401474d765015833bf653074
Dewang Patil <71816513+DewangPatil30@users.noreply.github.com>
2022-04-12 12:22:12 +0530

    Update 236: Lowest Common Ancestor of a Binary Tree.py

--- 236: Lowest Common Ancestor of a Binary Tree.py
+++ 236: Lowest Common Ancestor of a Binary Tree.py
@@ -2,16 +2,45 @@
 Approaches: 
         
         1. Brute Force: TC: O(N)    SC: O(N):
-            
             a. Find the root to node path of first node p and store in array
             b. Find the root to node path of second path q and store in another array
             c. Loop for min len b/w parray and qarray
             d. Return the last same node as ans
             Note: if no return in the loop than: return qarray[-1] if len(parray) > len(qarray) else parray[-1]
             
+            
+        2. DFS: TC: O(N)      SC: O(1)
+           
+           Intution: travas each path, if while travasing any node p or q is found no need of travasing further
+                     just return found root p or q else not found either return null
+            
+            a. if not root or root = p or q: return root
+            b. left = fn(root.left, p, q)
+               right = fn(root.right, p, q)
+            c. if not left: return right
+               if not right: return left
+               else: return root
+        
+            
      LeetCode 236: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
      YT: https://www.youtube.com/watch?v=_-QHfMDde90
 """
+###### DFS #######
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        if not root or root == p or root == q: 
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if not left: return right
+        if not right: return left
+        return root
+        
+
+
 # BRUTE FORCEEEEEEE
 
 class Solution:
