43bbe1211181e875b5913ec279dad8c37d330747
Dewang Patil <71816513+DewangPatil30@users.noreply.github.com>
2022-04-14 20:41:37 +0530

    Update 785: Check Bipartite Graph BFS.py

--- 785: Check Bipartite Graph BFS.py
+++ 785: Check Bipartite Graph BFS.py
@@ -16,11 +16,13 @@
                    if not than append in q as [adjnode, !color[cur node]]
                    and if colored is it same as cur node color
                    if yes than return 0
+        
+        2. Using DFS and Same logic as cycle detection in undirected graph:  TC: O(N+E)    SC: O(N): Same as above just apply recursion
 
 LeetCode 785: https://leetcode.com/problems/is-graph-bipartite/submissions/
 YT: https://www.youtube.com/watch?v=nbgaEu-pvkU&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=10
 """
-########### BFS ############
+###########  function for BFS ############
 def checkbpt(node):
     if color[node] == -1:
         color[node] = 0
@@ -33,15 +35,32 @@ def checkbpt(node):
             if color[i] == -1:
                 color[i] = 0 if c == 1 else 1
                 q.append([i, color[i]])
-            elif color[i] == c: return True
+            elif color[i] == c: return False
 
-    return False
+    return True
+#___________________________________________#
+########## function for DFS #############
+
+def checkbptDFS(node):        
+    if color[node] == -1:
+        color[node] = 0
+
+    for i in graph[node]:
+        if color[i] == -1:
+            color[i] = 1 - color[node]
+            if not checkbptDFS(i): return False
+        elif color[i] == color[node]: return False
+
+    return True
+#____________________________________________#
 
 color = [-1]*len(graph)
 color[0] = 0
 
 for i in range(len(graph)):
-    if checkbpt(i): return False
+    if not checkbptBFS(i): return False #For BFS
+    #if not checkbptBFS(i): return False #For DFS
+    
 
 return True
 
