2d26528f7155f514eaabe769b2c96b95940f62ad
Dewang Patil <71816513+DewangPatil30@users.noreply.github.com>
2022-04-15 01:57:49 +0530

    Update GFG: Topological Sort.py

--- GFG: Topological Sort.py
+++ GFG: Topological Sort.py
@@ -5,12 +5,22 @@
                       there can be multiple sort order for a graph
                       
          1. Topological Sort Using DFS:   TC: O(N + E)   SC: O(2N)  AUX: O(N)
-         
                Its nothing but travase the graph using DFS and after the end of each recursion 
                add the node in the stack, After whole travasal pop whole stack and push in another array 
                
-
+         2. Using BFS (Kanh's Algo):   TC: O(N + E)   SC: O(2N):
+                a. Count the indegrees of all the nodes
+                b. take q and append the all the nodes having indeg as 0
+                c. BFS and add node to res array as soon as its popped
+                d. check for nodes adj nodes and if present than decrement their indeg by 1 
+                    if the indegree becmes 0 after decrement than add to q 
+                    
+                 return res
+                 
+       GFG: https://practice.geeksforgeeks.org/problems/topological-sort/1#
+       YT: https://www.youtube.com/watch?v=rZv_jHZva34&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=14
 """
+############ DFS #############
 def topoSort(self, v, adj):
     def sort(node):
         visited[node] = True
@@ -29,4 +39,29 @@ def topoSort(self, v, adj):
 
     return res[::-1]
 
-
+############## BFS ############
+def topoSort(self, v, adj):
+        #Using BFS Kanh's Algo
+        
+        indeg = [0]*v       
+        for i in range(v):
+            for j in adj[i]:    #Counting Indegrees ie, no. of edges comming to node
+                indeg[j] += 1
+                
+        q = deque()             #take q and append all nodes having indeg 0
+        for i in range(v):
+            if indeg[i] == 0:
+                q.append(i)
+                
+        res = []        
+        while q:                #simple BFS
+            node = q.popleft()
+            res.append(node)
+            for i in adj[node]:
+                indeg[i] -= 1   #decrement Indeg of adj node and if 0 after, add to queue
+                if indeg[i] == 0:
+                    q.append(i)
+                    
+        return res
+    
+    
