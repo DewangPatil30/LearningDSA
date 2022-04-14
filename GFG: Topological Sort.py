331a88a9ee45a9884eb1a0395d56a043f857018e
Dewang Patil <71816513+DewangPatil30@users.noreply.github.com>
2022-04-15 01:58:34 +0530

    Update GFG: Topological Sort.py

--- GFG: Topological Sort.py
+++ GFG: Topological Sort.py
@@ -4,7 +4,7 @@
                       It is done in only **Directed Acyclic Graph (DAG)**
                       there can be multiple sort order for a graph
                       
-         1. Topological Sort Using DFS:   TC: O(N + E)   SC: O(2N)  AUX: O(N)
+         1. Using DFS:   TC: O(N + E)   SC: O(2N)  AUX: O(N)
                Its nothing but travase the graph using DFS and after the end of each recursion 
                add the node in the stack, After whole travasal pop whole stack and push in another array 
                
