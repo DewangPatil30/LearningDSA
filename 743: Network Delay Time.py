f0460b535e43801aa490f18d5a884da68a349c2e
Dewang Patil <71816513+DewangPatil30@users.noreply.github.com>
2022-04-17 01:54:53 +0530

    Update 743: Network Delay Time.py

--- 743: Network Delay Time.py
+++ 743: Network Delay Time.py
@@ -11,12 +11,24 @@
             
             return max(dist) if all( i != inf for i in dist) else -1
             
-        2. Dijkstrae Algo: TODO
- 
+        2. Dijkstrae Algo:  TC: (N+E) log N    sc: O(N)
+        
+            Actual Dijkstras algo implementation
+            just instead of heappushing (0, 1) push (0, k)    "it implies dist=0 and node=k"
+            
+            1. create adj mat and dist array same as detailed above
+            2. heappush (0, k) as (dist, k) in heap
+            3. BFS heappop   w, node = heappop(q)
+            4. for each adj node of node: n, w = adjNode, adjNodeWeight
+            5. Same as mentioned above
+            
+            
  LeetCode 743: https://leetcode.com/problems/network-delay-time/
- YT: https://www.youtube.com/watch?v=CrxG4WJotgg&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=17
+ YT BFS: https://www.youtube.com/watch?v=CrxG4WJotgg&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=17
+ YT Dijkstras Algo: https://www.youtube.com/watch?v=jbhuqIASjoM&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=18
 """
 
+########## BFS ####################
 class Signal:
     def __init__(self, node, w):
         self.node = node
@@ -48,4 +60,40 @@ class Solution:
                     
         if any(i == inf for i in dist): return -1
         return max(dist)
-                
+
+__________________________________________________________________________________________________________________________________________________________________
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+############## Dijkstras #######################
+
+from heapq import *
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        m = {i:[] for i in range(1, n+1)}
+        
+        for t in times:
+            m[t[0]].append([t[1], t[2]])
+
+            
+        dist = [inf]*(n+1)
+        dist[0] = dist[k] = 0
+        
+        q = []
+        heappush(q, (0,k))
+        
+        while q:
+            w, node = heappop(q)
+            for i in m[node]:
+                n, cw = i
+                d = w + cw
+                if dist[n] > d:
+                    dist[n] = d
+                    heappush(q, (d, n))
+                    
+                    
+        return -1 if any(i == inf for i in dist) else max(dist)
+                    
+        
+        
+
+  
